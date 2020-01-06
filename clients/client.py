0e027fccb2f5937accf6e64ed1e0e3c06d9caf67
Swoos <swoosdontknowme@gmail.com>
2020-01-06 13:13:55 +0100

    console active client

--- clients/client.py
+++ clients/client.py
@@ -1,18 +1,25 @@
 import socket
 import readline
+import sys
 
 HOST ='127.0.0.1'
 PORT = 65002
 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    while True:
-        send_str=input("Command:").encode('utf-8')
-        if send_str == b'exit':
-            break
-        s.sendall(send_str)
+    if sys.argv[1] == "-i":
+        while True:
+            send_str=input("Command:").encode('utf-8')
+            if send_str == b'exit':
+                break
+            s.sendall(send_str)
+            data = s.recv(1024)
+            print('Received:\r\n'+data.decode('utf-8'))
+            if data == b'Shutting down Server!' or data == b'Disconnected.':
+                break
+    else:
+        s.sendall(sys.argv[1].encode('utf-8'))
         data = s.recv(1024)
         print('Received:\r\n'+data.decode('utf-8'))
-        if data == b'Shutting down Server!' or data == b'Disconnected.':
-            break
         
